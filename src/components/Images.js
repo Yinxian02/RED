53def88c7b571a3ca281fed7ff7fb7617d0c2b8a
Yin <yxl21@ic.ac.uk>
2023-08-31 16:25:28 +0100

    rgb carousel

---
+++ src/components/Images.js
@@ -0,0 +1,19 @@
+const Images = [
+    {
+      id: 1,
+      url: 'http://localhost:3000/red dino.png',
+      alt: "red dino",
+    },
+    {
+      id: 2,
+      url : 'http://localhost:3000/green dino.png',
+      alt: "green dino",
+    },
+    {
+      id: 3,
+      url : 'http://localhost:3000/blue dino.png',
+      alt: "blue dino",
+    },
+  ];
+  
+  export default Images;
--- src/pages/RGBSchool.js
+++ src/pages/RGBSchool.js
@@ -1,22 +1,36 @@
 import React from 'react'
-import dinoCard from '../assets/red dino.png' 
+// import dinoCard from '../assets/red dino.png' 
 import '../styles/RGBSchool.css'
+import Slider from 'react-slick'
 
-class RGBSchool extends React.Component{
-    constructor(props) {
-        super(props);
-        this.imgRef= React.createRef();
-    }
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Images from '../components/Images'
 
-    render(){
-        return (
-          <>
-            <div className='dino-card-container'>
-                <img src={dinoCard} className="dino-card" ref={this.imgRef} alt="dino-card" />
-            </div>
-          </>
-        );
-    }
-}
+const RGBSchool = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <div className="content">
+        <div className="container">
+            <Slider {...settings}>
+                {Images.map((item) => (
+                    <div key={item.id}>
+                        <img src={item.url} alt={item.alt} className="img" />
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    </div>
+  );
+};
 
 export default RGBSchool;
