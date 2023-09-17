aa3f3423aef4b411c91571850c6745ef65334a6b
Yin <yxl21@ic.ac.uk>
2023-09-17 14:46:06 +0100

    log out added

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {NavLink, Outlet, Routes, Route} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 function Admin() {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
   const Navigation = () => {
     return (
       <nav>
+        {user && (
+          <div>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+
         <NavLink to='exercises-list'>List of Exercises</NavLink>
         <NavLink to='create-exercise'>Create Exercise</NavLink>
   
