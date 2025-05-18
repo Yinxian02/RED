dec01720136dac685dafed66c19f9da31a60576f
Sofia Liang <103280182+Yinxian02@users.noreply.github.com>
2025-05-18 22:52:55 +0100

    add project data and map setup

---
+++ src/components/ProjectsList.js
@@ -0,0 +1,64 @@
+const projectsList = [
+  {
+    id: 1,
+    name: "Kampung Bingolon, Kudat",
+    year: 2022,
+    latitude: 7.170929408104118,
+    longitude: 117.51978289942377,
+    paragraph: "The COVID-19 pandemic paused RED’s annual operations for two years (2020–2021), with no summer expeditions and limited term-time activities. In 2022, as restrictions eased and our project was approved, we were excited to finally return to the field. Despite a leaner team with fewer experienced volunteers, we successfully carried out a rewarding expedition—thanks to countless discussions, creative problem-solving, and the team’s unwavering dedication.",
+    reportLink: "https://drive.google.com/file/d/1V27RwjMI7j7GvgLo04eWF6Wl0ACRtoHe/view?usp=sharing",
+    youtubeLink: "https://www.youtube.com/embed/KpyeHYGpz1w?autoplay=1mute=1"
+  },
+  {
+    id: 2,
+    name: "Kampung Taburan Tengah, Kota Belud",
+    year: 2019,
+    latitude: 6.612527533890887,
+    longitude: 116.58627771919912,
+    paragraph: "Drawing from the past three experiences, the 2019 team developed strategies to improve and optimise building design and construction efficiency.",
+    reportLink: "https://drive.google.com/file/d/1knu_uLdd7c2jrgk3aAjsvYN4xfuFB20u/view?usp=sharing",
+    youtubeLink: "https://www.youtube.com/watch?v=suBQiROM2JU"
+  },
+  {
+    id: 3,
+    name: "Kampung Kuala Punteh, Keningau",
+    year: 2018,
+    latitude: 5.239533847545068,
+    longitude: 116.14600636149383,
+    paragraph: "The 2018 RED team continued to touch the lives of the rural community at Kampung Kuala Punteh by constructing an education centre, replicating the original design.",
+    reportLink: "https://drive.google.com/file/d/1p5TiVIcRLK49fwH9fPP2ghU64t-RPQIp/view?usp=sharing",
+    youtubeLink: "https://www.youtube.com/watch?v=-QX64g7fW1Q"
+  },
+  {
+    id: 4,
+    name: "Kampung Palipikan, Kota Marudu",
+    year: 2017,
+    latitude: 6.378199159444044,
+    longitude: 116.77795510503549,
+    paragraph: "Following the success of the pilot project, the 2017 team of nineteen volunteers replicated the design of the first building in Kampung Palipikan, completing construction in five weeks. An off-grid solar system was also incorporated for electricity supply.",
+    reportLink: "https://drive.google.com/file/d/1GQjkCTR2CAD4JX_x7D2EdfQoCKuOzONI/view?usp=sharing",
+    youtubeLink: "https://www.youtube.com/watch?v=Stac1f9JSY4"
+  },
+  {
+    id: 5,
+    name: "Kampung Gaur, Ranau",
+    year: 2016,
+    latitude: 5.870029537730061,
+    longitude: 116.66806865151605,
+    paragraph: "Building on the feasibility studies, RED launched its pilot project in Kampung Gaur with fourteen volunteers from Imperial College London. A single storey 6m x 9m education centre was constructed in 6 weeks, equipped with a rainwater harvesting tank.",
+    reportLink: null,
+    youtubeLink: null
+  },
+  {
+    id: 6,
+    name: "Kampung Indrasan, Kudat",
+    year: 2015,
+    latitude: 6.763818234884976,
+    longitude: 116.64048565898867,
+    paragraph: "RED founders, Edrea, Chloe, and Jack, conducted a three-week feasibility study at Kampung Indrasan. Through helping a local community build an education centre, they assessed the region’s climate, geological conditions, and availability of locally and sustainably sourced construction material. A needs assessment was carried out to address educational demands from the rural communities in Borneo.",
+    reportLink: null,
+    youtubeLink: null
+  }
+]
+
+export default projectsList;
--- src/pages/PastProjects.js
+++ src/pages/PastProjects.js
@@ -1,37 +1,39 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import '../styles/PastProjects.css'
+import projectsList from '../components/ProjectsList.js'
 
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-// const mapContainerStyle = {
-//   height: '400px',
-//   width: '100%'
-// };
+const mapContainerStyle = {
+  height: '500px',
+  width: '100%'
+};
 
-// const center = {
-//   lat: 4.399068,
-//   lng: 111.270459
-// };
-
-// const locations = [
-//   { id: 1, lat: 7.170929408104118, lng: 117.51978289942377, name: 'Tadika Kami' },
-// ];
+const center = {
+  lat: 6.378199159444044,
+  lng: 116.77795510503549,
+};
 
 function PastProjects() {
   return (
     <>
-    {/* <div className='past-projects-div'>
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          center={center}
-          zoom={5}
-        >
-          {locations.map(loc => (
-            <Marker key={loc.id} position={{ lat: loc.lat, lng: loc.lng }} />
-          ))}
-        </GoogleMap>
-      </LoadScript>
-    </div> */}
+    <div className='past-projects-div'>
+      <div className='google-map-div'>
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={center}
+            zoom={8}
+          >
+            {projectsList.map(loc => (
+              <Marker key={loc.id} position={{ lat: loc.latitude, lng: loc.longitude }} />
+            ))}
+          </GoogleMap>
+        </LoadScript>
+      </div>
+      <div className='project-details-div'>
+
+      </div>
+    </div>
     </>
   );
 }
